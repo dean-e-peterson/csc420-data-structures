d027ca200c4f24a7f034abb35f03e1a2864f55fc
Dean E. Peterson <dean.e.penguin@gmail.com>
2019-10-26 19:33:26 -0500

    Assignment 1 initial project

---
+++ Assign1/src/Assign1.java
@@ -0,0 +1,19 @@
+/**
+ * Assign1.java
+ * Concordia CSC420 Data Structures
+ * Assignment 1 - Random Name Generator
+ * Proof of concept to generate fake names for test environments.
+ * By Dean Peterson <petersod8@csp.edu>
+ * Instructor James Tucker
+ * October 26, 2019
+ */
+public class Assign1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
