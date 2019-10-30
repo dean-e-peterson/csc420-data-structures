411cb02ee4338106f72e30d134439c0a4d46f08e
Dean E. Peterson <dean.e.penguin@gmail.com>
2019-10-29 21:34:15 -0500

    Assignment 2 project and starter Band class

---
+++ Assign2/src/Assign2.java
@@ -0,0 +1,20 @@
+/*
+ * Assign2.java
+ * Concordia CSC420 - Data Structures
+ * Assignment 2 - Band Coordinator - Main program
+ * App to search bands by set length.
+ * By Dean Peterson <petersod8@csp.edu>
+ * Instructor James Tucker
+ * October 29, 2019
+ */
+
+public class Assign2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Submitted by: Dean Peterson - petersod8@csp.edu");
+    }
+    
+}
---
+++ Assign2/src/Band.java
@@ -0,0 +1,48 @@
+/*
+ * Band.java
+ * Concordia CSC420 - Data Structures
+ * Assignment 2 - Band Coordinator - Band class
+ * Represents a band tracked by the application, holding name and set length.
+ * By Dean Peterson <petersod8@csp.edu>
+ * Instructor James Tucker
+ * October 29, 2019
+ */
+
+class Band {
+    
+    private static final char DELIMITER = '|';
+    
+    private String bandName;
+    private float setTime; // Length of band's musical set in minutes.
+    
+    public Band(String bandName, float setTime) {
+        this.bandName = bandName;
+        this.setTime = setTime;
+    }
+    
+    public static Band deserialize(String bandString) {
+        int delimiterPos = bandString.indexOf(DELIMITER);
+        String bandName = bandString.substring(0, delimiterPos);
+        String setTime = bandString.substring(delimiterPos + 1, bandString.length());
+        return new Band(bandName, Float.valueOf(setTime));
+    }
+
+    public String getBandName() {
+        return bandName;
+    }
+    public float getSetTime() {
+        return setTime;
+    }
+    public void setBandName(String bandName) {
+        this.bandName = bandName;
+    }
+    public void setSetTime(float setTime) {
+        this.setTime = setTime;
+    }
+    
+    @Override
+    public String toString() {
+        return bandName + " has a set time of " + setTime + " minutes";
+    }
+    
+}
